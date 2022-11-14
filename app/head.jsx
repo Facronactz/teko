b027756f9b5c5ebee4f83340b4d198d7ecb43002
Facronactz <farroaxza@gmail.com>
2022-11-14 19:55:16 +0700

    upload

--- app/head.jsx
+++ app/head.jsx
@@ -1,10 +1,7 @@
-import React from "react";
-
 export default function Head() {
   return (
     <>
       <title>Teko</title>
-      <meta charSet="UTF-8" />
       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="description" content="Teko" />
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,4 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import './globals.css'
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
