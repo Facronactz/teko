df2d46b72b04e24b0487817d270879fd4b971ad8
Rifqi Alamsyah <rifqialamsyh@gmail.com>
2022-11-17 21:43:39 +0700

    add back button in daftar page

--- app/daftar/page.jsx
+++ app/daftar/page.jsx
@@ -7,6 +7,7 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
 import formBg from "../../public/image/formBg.jpg";
+import lArrow from "../../public/image/leftarrow.png";
 
 export default function daftar() {
   return (
@@ -15,6 +16,16 @@ export default function daftar() {
         <Container className="bg-white border rounded p-0 w-3/6 my-[150px]">
           <div className="flex flex-row">
             <div className="flex flex-col mr-7 relative">
+              <Link
+                href={"/"}
+                className="w-10 h-10 rounded bg-white absolute m-3 "
+              >
+                <Image
+                  src={lArrow}
+                  className="w-3/6 m-auto mt-2"
+                  alt="gambar hero"
+                />
+              </Link>
               <h1 className="font-extrabold text-base text-left w-1/3 absolute bottom-[8%] left-[8%] text-white xs:text-xl sm:text-2xl md:text-5xl xxl:text-6xl">
                 Selamat Datang
               </h1>
@@ -23,7 +34,7 @@ export default function daftar() {
             <div className="flex flex-col py-3">
               <Form>
                 <Form.Group className="mb-3" controlId="formBasicNama">
-                  <Form.Label>Nama</Form.Label>
+                  <Form.Label>Masukan nama</Form.Label>
                   <Form.Control type="email" placeholder="Masukan Nama" />
                   <Form.Text className="text-muted"></Form.Text>
                 </Form.Group>
