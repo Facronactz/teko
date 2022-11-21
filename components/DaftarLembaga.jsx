5c7a7da80e742f743e1b5e9870cde5a617d343e6
Facronactz <farroaxza@gmail.com>
2022-11-21 15:59:44 +0700

    daftar modal

---
+++ components/DaftarLembaga.jsx
@@ -0,0 +1,90 @@
+'use client';
+
+import Link from 'next/link';
+import Image from 'next/image';
+
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+import formBg from '../public/image/formBg.jpg';
+import lArrow from '../public/image/leftarrow.png';
+
+export default function DaftarLembaga() {
+  return (
+    <>
+      <div>
+        <Container className="bg-white border rounded p-0 w-4/5 my-[20px] xl:my-[150px] xl:w-3/5">
+          <div className="flex flex-col xl:flex-row">
+            <div className="flex flex-col xl:mr-7 relative">
+              <Link
+                href={'/'}
+                className="w-10 h-10 rounded bg-white absolute m-3 "
+              >
+                <Image
+                  src={lArrow}
+                  className="w-3/6 m-auto mt-2"
+                  alt="gambar hero"
+                />
+              </Link>
+              <h1 className="font-extrabold text-3xl text-left w-1/3 absolute bottom-[8%] left-[8%] text-white md:text-5xl xxl:text-6xl">
+                Selamat Datang
+              </h1>
+              <Image
+                className="w-full h-[200px] xl:h-full"
+                src={formBg}
+                alt="gambar hero"
+              />
+            </div>
+            <div className="flex flex-col py-3">
+              <Form>
+                <Form.Group className="my-2 mx-3" controlId="formBasicNama">
+                  <Form.Label>Masukan nama Lembaga</Form.Label>
+                  <Form.Control type="email" placeholder="Masukan Nama" />
+                  <Form.Text className="text-muted"></Form.Text>
+                </Form.Group>
+                <Form.Group className="my-2 mx-3" controlId="formBasicEmail">
+                  <Form.Label>Alamat Email</Form.Label>
+                  <Form.Control type="email" placeholder="Masukan Email" />
+                  <Form.Text className="text-muted">
+                    Well never share your email with anyone else.
+                  </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="my-2 mx-3" controlId="formBasicPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Form.Group className="my-2 mx-3" controlId="formBasicCheckbox">
+                  <Form.Check type="checkbox" label="Kemanusiaan" />
+                </Form.Group>
+                <Form.Group className="my-2 mx-3" controlId="formBasicCheckbox">
+                  <Form.Check type="checkbox" label="Sosial" />
+                </Form.Group>
+                <Form.Group className="my-2 mx-3" controlId="formBasicCheckbox">
+                  <Form.Check type="checkbox" label="Lingkungan" />
+                </Form.Group>
+                <Button
+                  type="submit"
+                  className="bg-brand border-brand hover:bg-[#14532d] my-2 mx-3"
+                >
+                  Daftar
+                </Button>
+                <p className="my-2 mx-3">
+                  Daftar sebagai Pengguna Umum,
+                  <br /> <br />
+                  <a
+                    href="/daftarumum"
+                    className="no-underline text-white p-2 bg-brand rounded"
+                  >
+                    Klik disini
+                  </a>
+                </p>
+              </Form>
+            </div>
+          </div>
+        </Container>
+      </div>
+    </>
+  );
+}
--- components/daftarmodals.jsx
+++ components/daftarmodals.jsx
@@ -1,41 +1,111 @@
 'use client';
 
-import { useState } from 'react';
+// import { useState } from 'react';
+
+import React from 'react';
 
 import Link from 'next/link';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Modal from 'react-bootstrap/Modal';
 
-function DaftarModal() {
-  const [show, setShow] = useState(false);
+import DaftarUmum from './DaftarUmum';
+import DaftarLembaga from './DaftarLembaga';
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+// function DaftarModal() {
+//   const [show, setShow] = useState(false);
 
-  return (
-    <>
-      <Link
-        href="#"
-        onClick={handleShow}
-        className="no-underline font-semibold text-brand px-4 py-2 border border-1 border-brand md:mr-6 hover:bg-[#f8fafc] rounded"
-      >
-        Daftar
-      </Link>
+//   const handleClose = () => setShow(false);
+//   const handleShow = () => setShow(true);
 
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Daftar Sebagai:</Modal.Title>
-        </Modal.Header>
-        <Modal.Body className=" flex justify-center">
-          <ButtonGroup size="lg" className="m-2 bg-brand">
-            <Button href="/daftarumum">Umum</Button>
-            <Button href="/daftarlembaga">Lembaga</Button>
-          </ButtonGroup>
-        </Modal.Body>
-      </Modal>
-    </>
-  );
+//   const [showUmum, setShowUmum] = useState(false);
+//   const handleCloseUmum = () => setShowUmum(false);
+//   const handleShowUmum = () => setShowUmum(true);
+
+//   const [showLembaga, setShowLembaga] = useState(false);
+//   const handleCloseLembaga = () => setShowLembaga(false);
+//   const handleShowLembaga = () => setShowLembaga(true);
+
+//   return (
+//     <>
+//       <Link
+//         href="#"
+//         onClick={handleShow}
+//         className="no-underline font-semibold text-brand px-4 py-2 border border-1 border-brand md:mr-6 hover:bg-[#f8fafc] rounded"
+//       >
+//         Daftar
+//       </Link>
+
+//       <Modal show={show} onHide={handleClose}>
+//         <Modal.Header closeButton>
+//           <Modal.Title>Daftar Sebagai:</Modal.Title>
+//         </Modal.Header>
+//         <Modal.Body className=" flex justify-center">
+//           <ButtonGroup size="lg" className="m-2 bg-brand">
+//             <Button href="#" onclick={handleShowUmum && handleCloseLembaga}>Umum</Button>
+//             <Button href="#" onclick={handleShowLembaga && handleCloseUmum}>Lembaga</Button>
+//           </ButtonGroup>
+//           {showUmum && <DaftarUmum />}
+//           {showLembaga && <DaftarLembaga />}
+//         </Modal.Body>
+//       </Modal>
+//     </>
+//   );
+// }
+
+// export default DaftarModal;
+
+class DaftarModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false,
+      showUmum: false,
+      showLembaga: false,
+    };
+  }
+
+  handleClose = () => this.setState({ show: false });
+
+  handleShow = () => this.setState({ show: true });
+
+  showUmum = () => {
+    this.setState({ showUmum: true });
+    this.setState({ showLembaga: false });
+  };
+
+  showLembaga = () => {
+    this.setState({ showLembaga: true });
+    this.setState({ showUmum: false });
+  };
+
+  render() {
+    return (
+      <>
+        <Link
+          href="#"
+          onClick={this.handleShow}
+          className="no-underline font-semibold text-brand px-4 py-2 border border-1 border-brand md:mr-6 hover:bg-[#f8fafc] rounded"
+        >
+          Daftar
+        </Link>
+
+        <Modal show={this.state.show} onHide={this.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Daftar Sebagai:</Modal.Title>
+          </Modal.Header>
+          <Modal.Body className="flex justify-center flex-col">
+            <ButtonGroup size="lg" className="m-2">
+              <Button className="bg-brand hover:bg-blue-500" href="#" onClick={this.showUmum}>Umum</Button>
+              <Button className="bg-brand hover:bg-white" href="#" onClick={this.showLembaga}>Lembaga</Button>
+            </ButtonGroup>
+            {this.state.showUmum && (<DaftarUmum />)}
+            {this.state.showLembaga && <DaftarLembaga />}
+          </Modal.Body>
+        </Modal>
+      </>
+    );
+  }
 }
 
 export default DaftarModal;
