d0c93a100530f77c5ed571ade64d51c90357307a
Rifqi Alamsyah <rifqialamsyh@gmail.com>
2022-11-24 15:26:38 +0700

    add detail-kegiatan & react icons

---
+++ app/detail-kegiatan/page.jsx
@@ -0,0 +1,96 @@
+'use client';
+
+import Image from 'next/image';
+
+import Container from 'react-bootstrap/Container';
+// import Button from 'react-bootstrap/Button';
+// import Form from 'react-bootstrap/Form';
+// import InputGroup from 'react-bootstrap/InputGroup';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
+// import Card from 'react-bootstrap/Card';
+
+import CustomNavbar from '../../components/navbar';
+import formBg from '../../public/image/formBg.jpg';
+
+class TemanPage extends React.Component {
+  render() {
+    return (
+      <>
+        <CustomNavbar></CustomNavbar>
+        <h3 className="text-center mb-3 font-bold">Detail Kegiatan</h3>
+        <Container className="bg-red flex flex-col justify-center">
+          <Image
+            className="w-full h-[200px] lg:w-2/5 lg:h-full mb-3"
+            src={formBg}
+            alt="gambar hero"
+          />
+          <h3>Nama Kegiatan</h3>
+          <h5 className="font-light text-sm">Dibuat oleh: Jaya ABadi</h5>
+          <p className="text-justify">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum
+          </p>
+        </Container>
+        <Container>
+          <h3 className="flex justify-center">Kontak</h3>
+          {/* <FontAwesomeIcon icon="fa-brands fa-square-whatsapp" /> */}
+        </Container>
+
+        {/* <main className="mx-5 mt-3">
+        <InputGroup className="">
+          <Form.Control
+            placeholder="Cari Teman"
+            aria-label="Cari Teman"
+            aria-describedby="basic-addon2"
+          />
+          <Button
+            id="button-addon2"
+            className="px-4 py-2 bg-brand border-brand"
+          >
+            Cari
+          </Button>
+        </InputGroup>
+      </main>
+
+      <section>
+        <Container className="grid p-0">
+          <Row className=" grid m-4 gap-4 s:grid-cols-2 lg:grid-cols-4">
+            {[1, 2, 3, 4, 5, 6, 7, 8].map((item) => (
+              <Col className="p-0" key={item}>
+                <Card>
+                  <Card.Img
+                    variant="top"
+                    src="https://via.placeholder.com/200"
+                    width="200"
+                    height="200"
+                    alt="..."
+                  />
+                  <Card.Body>
+                    <Card.Title>Nama </Card.Title>
+                    <Card.Text>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the cards content.
+                    </Card.Text>
+                    <Button className="bg-brand border-brand">Lihat</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </section> */}
+      </>
+    );
+  }
+}
+
+export default TemanPage;
+
