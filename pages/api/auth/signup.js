8f55285d88578473c8bc8ce4881e69c050f5f587
Facronactz <farroaxza@gmail.com>
2022-11-26 22:28:21 +0700

    add lembaga in db

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -24,15 +24,23 @@ export default async function handler(req, res) {
         }
     }
     const hashedPassword = await bcrypt.hash(password, 10);
-    const user = await prisma.user.create({
-        data: {
-            name,
-            email,
-            password: hashedPassword,
-            role,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-        },
-    });
-    res.status(200).json({ message: 'User created', user });
+    try {
+        const user = await prisma.user.create({
+            data: {
+                name,
+                email,
+                password: hashedPassword,
+                role,
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            },
+        });
+        res.status(200).json({ message: 'User created', user });
+    } catch (error) {
+        if (error.code === 'P2002') {
+            res.status(409).json({ message: 'Email already in use' });
+        } else {
+            res.status(500).json({ message: 'Internal server error' });
+        }
+    }
 }
