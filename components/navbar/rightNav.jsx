c00c0724f3000ba3a1acd416a2456b90ea6dfd9b
Facronactz <farroaxza@gmail.com>
2022-11-26 11:49:08 +0700

    fix no-unused-var

---
+++ components/navbar/rightNav.jsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { useSession } from 'next-auth/react';
+import Link from 'next/link';
+import DaftarModal from './daftarModals';
+import MasukModal from './masukModals';
+
+export default function RightNav() {
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <>
+        <Link href="/dashboard" className="mx-2 font-bold no-underline text-brand lg:text-lg">
+          Dashboard
+        </Link>
+        <Link href="/api/auth/signout" className="mx-2 font-bold no-underline text-brand lg:text-lg">
+          Keluar
+        </Link>
+      </>
+    );
+  }
+  return (
+    <>
+      <DaftarModal></DaftarModal>
+      <MasukModal></MasukModal>
+    </>
+  );
+}
