102ea2c721b430fbe27abb2106ace1c960c95f06
Rifqi Alamsyah <rifqialamsyh@gmail.com>
2022-12-05 19:45:57 +0700

    config teman-config

--- app/dashboard/admin/teman-config/page.jsx
+++ app/dashboard/admin/teman-config/page.jsx
@@ -1,21 +1,69 @@
 'use client';
 
 import { assetPrefix } from '@teko/next.config';
-import { use } from 'react';
 
 import Link from 'next/link';
 
 import { Container, Table, Button } from 'react-bootstrap';
+
+import Fetcher from '@teko/helpers/fetcher';
+import useSWR from 'swr';
+import Skeleton from 'react-loading-skeleton';
 
 import { GiThreeFriends } from 'react-icons/gi';
 import { ImCross } from 'react-icons/im';
 import { AiFillEdit } from 'react-icons/ai';
 import { GoPlus } from 'react-icons/go';
 
-const getTemans = async () => {
-  const data = await fetch(`${assetPrefix}/api/teman`, { cache: 'no-cache' });
-  return data.json();
-};
+const temanFetcher = new Fetcher({ url: 'teman' });
+function ShowTeman() {
+  const { data, error } = useSWR(
+    temanFetcher.url,
+    temanFetcher.fetcher,
+    temanFetcher.swrConfig
+  );
+  if (error) return <div>Gagal untuk memuat</div>;
+  if (!data) {
+    return (
+      <tr>
+        <td className="text-center">
+          <Skeleton />
+        </td>
+        <td className="text-center">
+          <Skeleton />
+        </td>
+        <td className="text-center">
+          <Skeleton />
+        </td>
+        <td className="flex flex-row justify-center">
+          <Link href={'#'} className="rounded ml-3 my-auto p-2">
+            <Skeleton width={60} height={30} />
+          </Link>
+          <Link href={'#'} className="ml-3 rounded">
+            <Skeleton width={60} height={30} />
+          </Link>
+        </td>
+      </tr>
+    );
+  }
+  return data.map((teman) => (
+    <tr key={teman.id}>
+      <td className="text-center">{teman.id}</td>
+      <td className="text-center">{teman.nama}</td>
+      <td className="flex flex-row justify-center">
+        <Link
+          href={`/dashboard/admin/teman-config/${teman.id}`}
+          className="bg-brand text-white border-brand rounded ml-3 my-auto p-2"
+        >
+          <AiFillEdit className="h-[25px] w-[70px] text-white" />
+        </Link>
+        <Button onClick={deleteTeman} className="bg-white border-brand ml-3">
+          <ImCross className="h-[25px] w-[70px] text-danger" />
+        </Button>
+      </td>
+    </tr>
+  ));
+}
 
 const deleteTeman = async () => {
   const res = await fetch(`${assetPrefix}/api/teman`, {
@@ -29,7 +77,6 @@ const deleteTeman = async () => {
 };
 
 export default function TemanConfig() {
-  const temans = use(getTemans());
   return (
     <>
       <Container className="m-0 p-0 w-52 bg-brand fixed h-full overflow-auto">
@@ -60,8 +107,12 @@ export default function TemanConfig() {
       <Container className="ml-52 mr-10 p-4 h-full">
         <h2 className="flex flex-row">
           <GiThreeFriends className="mr-2" /> Teman Config
-          {/* TODO button new teman ganti link dan arahain ke page new */}
-          <Button>New Teman <GoPlus></GoPlus></Button>
+          <Link
+            href={'/dashboard/admin/teman-config/new'}
+            className="no-underline flex flex-row text-white bg-brand p-2 my-auto ml-3 text-lg rounded"
+          >
+            New Teman <GoPlus></GoPlus>
+          </Link>
         </h2>
         <Table striped bordered hover>
           <thead>
@@ -72,26 +123,7 @@ export default function TemanConfig() {
             </tr>
           </thead>
           <tbody>
-            {temans.map((teman) => (
-              <tr key={teman.id}>
-                <td className="text-center">{teman.id}</td>
-                <td className="text-center">{teman.nama}</td>
-                <td className="flex flex-row justify-center">
-                  <Link
-                    href={`/dashboard/admin/teman-config/${teman.id}`}
-                    className="bg-brand text-white border-brand rounded ml-3 my-auto p-2"
-                  >
-                    <AiFillEdit className="h-[25px] w-[70px] text-white" />
-                  </Link>
-                  <Button
-                    onClick={deleteTeman}
-                    className="bg-white border-brand ml-3"
-                  >
-                    <ImCross className="h-[25px] w-[70px] text-danger" />
-                  </Button>
-                </td>
-              </tr>
-            ))}
+            <ShowTeman />
           </tbody>
         </Table>
       </Container>
