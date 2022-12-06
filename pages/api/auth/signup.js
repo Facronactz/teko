73c8a5a0a5a8957cdb537cf6a6dabb1ecb8d2f69
Facronactz <farroaxza@gmail.com>
2022-12-06 14:48:37 +0700

    add error in signup

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -38,9 +38,9 @@ export default async function SignUpHandler(req, res) {
         res.status(200).json({ message: 'User created', user });
     } catch (error) {
         if (error.code === 'P2002') {
-            res.status(409).json({ message: 'Email already in use' });
+            res.status(409).json({ message: 'Email already in use', error });
         } else {
-            res.status(500).json({ message: 'Internal server error' });
+            res.status(500).json({ message: 'Internal server error', error });
         }
     }
 }
