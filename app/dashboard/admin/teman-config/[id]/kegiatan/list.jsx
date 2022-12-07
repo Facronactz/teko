214003f9ec1d9c51f3c14545990ed7f37a5b8086
Rifqi Alamsyah <rifqialamsyh@gmail.com>
2022-12-07 15:12:30 +0700

    add edit & list for kegiatan page

--- app/dashboard/admin/teman-config/[id]/kegiatan/list.jsx
+++ app/dashboard/admin/teman-config/[id]/kegiatan/list.jsx
@@ -1,6 +1,34 @@
-// TODO
-export default function Page() {
+'use client';
+
+import { Table } from 'react-bootstrap';
+
+export default function ListKegiatanPage() {
   return (
-    <>Belum dibuat</>
+    <>
+      <Table striped bordered hover>
+        <thead className="text-center">
+          <tr>
+            <th>No</th>
+            <th>Nama Kegiatan</th>
+            <th>Tanggal</th>
+            <th>Aktif</th>
+          </tr>
+        </thead>
+        <tbody className="text-center">
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>
+              <input
+                type="checkbox"
+                className="w-6 h-6 rounded-lg"
+                value={'true'}
+              />
+            </td>
+          </tr>
+        </tbody>
+      </Table>
+    </>
   );
 }
